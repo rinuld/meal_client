a4ade3f4065410c85d9232cf0621abca1241dc49
rinuld <rjcanlas21@gmail.com>
2024-10-17 13:54:56 +0800

    added location, name, and activity date

--- src/components/ActivityReportPrint.js
+++ src/components/ActivityReportPrint.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useContext } from "react";
 import '../assets/css/ActivityReportPrint.css';
 import logo from '../assets/images/logo.png';
+import AuthContext from "../context/AuthProvider";
 
 const ActivityReportPrint = ({ PDFData, activityReportID }) => {
 
     console.log('PDF DATA:', PDFData);
-
+    const { auth } = useContext(AuthContext);
     const disaggregatedData = PDFData.genderAgeDisabilityData;
 
     const formatDate = (date) => {
@@ -14,75 +16,111 @@ const ActivityReportPrint = ({ PDFData, activityReportID }) => {
     };
 
     const currentDate = formatDate(new Date());
+    const activityDate = formatDate(PDFData.activityDate);
 
     return (
         <div id="formToPrint">
-            <div className="voucher-nyli-logo">
-                <img src={logo} alt="NYLI" />
-                <div className="right-title">
-                    <h3>Activity Report</h3>
-                    <p>{activityReportID} - {currentDate}</p>
-                </div>
+            <div className="nyli-logo">
+                <img src={logo} alt="NYLI"/>
             </div>
-
-            <br />
-            <div className="row gx-3">
-                <div className="col-6">
-                    <p><strong style={{ color: '#106544' }}>Project:</strong> {PDFData.selectedProject}</p>
-                </div>
-                <div className="col-6">
-                    <p><strong style={{ color: '#106544' }}>Activity:</strong> {PDFData.selectedActivity}</p>
-                </div>
-            </div>
-
-            <div className="row gx-3">
+            <div className="nyli-logo">
                 <div className="col-12">
+                    <p><strong>Rapid Post-Activity Report</strong> - {activityReportID}</p>
+                </div>     
+            </div>
+
+            <br/>
+            
+            <div className="center">
+                <div className="row">
+                    <div className="col-6">
+                        <p><strong style={{ color: '#106544' }}>Activity Title:</strong> {PDFData.selectedActivity}</p>
+                    </div>
+                    <div className="col-6">
+                        <p><strong style={{ color: '#106544' }}>Project:</strong> {PDFData.selectedProject}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <p><strong style={{ color: '#106544' }}>Date of the Activity:</strong> {activityDate}</p>
+                    </div>
+                    <div className="col-6">
+                        <p><strong style={{ color: '#106544' }}>Location:</strong> {PDFData.location}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <p><strong style={{ color: '#106544' }}>Prepared by:</strong> {auth.firstname} {auth.lastname} </p>
+                    </div>
+                    <div className="col-6">
+                        <p><strong style={{ color: '#106544' }}>Date Prepared:</strong> {currentDate}</p>
+                    </div>
+                </div>
+            </div>
+
+            <br/>
+
+            <div className="center">
+                <div className="row">
                     <p><strong style={{ color: '#106544' }}>Objective:</strong> {PDFData.selectedObjective}</p>
                 </div>
-            </div>
-            <div className="row gx-3">
-                <div className="col-12">
+                <div className="row">
                     <p><strong style={{ color: '#106544' }}>Outcome:</strong> {PDFData.selectedOutcome}</p>
                 </div>
-            </div>
-            <div className="row gx-3">
-                <div className="col-12">
+                <div className="row">
                     <p><strong style={{ color: '#106544' }}>Output:</strong> {PDFData.selectedOutput}</p>
                 </div>
-            </div>
-            <div className="row gx-3">
-                <div className="col-12">
+                <div className="row">
                     <p><strong style={{ color: '#106544' }}>Indicator:</strong> {PDFData.selectedIndicator}</p>
                 </div>
             </div>
 
-            <br />
-            <div className="row gx-3 mb-3">
-                <div className="col-12">
-                    <p><strong style={{ color: '#106544' }}>Institutions and Institutional Groups</strong></p>
+            <br/>
+
+            <div className='center'>
+                <div className="row">
+                    <p><strong style={{ color: '#106544' }}>Detailed Description of the Activity:</strong></p>
+                    <p>{PDFData.detailedDescription}</p>
+                </div>
+                <div className="row">
+                    <p><strong style={{ color: '#106544' }}>Key Outputs and Results:</strong></p>
+                    <p>{PDFData.keyOutputs}</p>
+                </div>
+                <div className="row">
+                    <p><strong style={{ color: '#106544' }}>Challenges and Lessons Learned:</strong></p>
+                    <p>{PDFData.challenges}</p>
+                </div>
+            </div>
+
+            <div style={{ pageBreakAfter: 'always' }}></div> {/* Page break here */}
+            
+            <div className="center">
+                <div className="row">
+                    <p><strong style={{ color: '#106544' }}>Institutions and Institutional Groups:</strong></p>
                     <p>{PDFData.selectedInstitutions.join(', ')}</p>
                 </div>
             </div>
-            <div className="row gx-3 mb-3">
-                <div className="col-12">
+
+            <div className="center">
+                <div className="row">
                     <p><strong style={{ color: '#106544' }}>Gender, Age, and Disability Disaggregated Data</strong></p>
                     {/* Render the table here */}
                     <table className="disaggregated-data-table" style={{ width: '100%', borderCollapse: 'collapse' }}>
                         <thead>
                             <tr>
-                                <th style={{ border: '1px solid #000', padding: '8px' }}>Persons Involved</th>
-                                <th style={{ border: '1px solid #000', padding: '8px' }}>Male Participants</th>
-                                <th style={{ border: '1px solid #000', padding: '8px' }}>Female Participants</th>
-                                <th style={{ border: '1px solid #000', padding: '8px' }}>Members of the LGBTQIA+ Community</th>
+                                <th style={{ border: '1px solid #106544', padding: '10px' }}>Persons Involved</th>
+                                <th style={{ border: '1px solid #106544', padding: '10px' }}>Male Participants</th>
+                                <th style={{ border: '1px solid #106544', padding: '10px' }}>Female Participants</th>
+                                <th style={{ border: '1px solid #106544', padding: '10px' }}>Members of the LGBTQIA+ Community</th>
                             </tr>
                         </thead>
                         <tbody>
                             {disaggregatedData.map((item, index) => (
                                 <tr key={index}>
-                                    <td style={{ border: '1px solid #000', padding: '8px' }}>{item.category}</td>
-                                    <td style={{ border: '1px solid #000', padding: '8px' }}>{item.male}</td>
-                                    <td style={{ border: '1px solid #000', padding: '8px' }}>{item.female}</td>
-                                    <td style={{ border: '1px solid #000', padding: '8px' }}>{item.lgbtqia}</td>
+                                    <td style={{ border: '1px solid #106544', padding: '10px' }}>{item.category}</td>
+                                    <td style={{ border: '1px solid #106544', padding: '10px' }}>{item.male}</td>
+                                    <td style={{ border: '1px solid #106544', padding: '10px' }}>{item.female}</td>
+                                    <td style={{ border: '1px solid #106544', padding: '10px' }}>{item.lgbtqia}</td>
                                 </tr>
                             ))}
                         </tbody>
@@ -92,33 +130,14 @@ const ActivityReportPrint = ({ PDFData, activityReportID }) => {
 
             <div style={{ pageBreakAfter: 'always' }}></div> {/* Page break here */}
 
-            <br />
-            <div className="row gx-3 mb-3">
-                <div className="col-12">
-                    <p><strong style={{ color: '#106544' }}>Detailed Description of the Activity:</strong></p>
-                    <p>{PDFData.detailedDescription}</p>
-                </div>
-            </div>
-            <div className="row gx-3 mb-3">
-                <div className="col-12">
-                    <p><strong style={{ color: '#106544' }}>Key Outputs and Results:</strong></p>
-                    <p>{PDFData.keyOutputs}</p>
-                </div>
-            </div>
-            <div className="row gx-3 mb-3">
-                <div className="col-12">
-                    <p><strong style={{ color: '#106544' }}>Challenges and Lessons Learned:</strong></p>
-                    <p>{PDFData.challenges}</p>
-                </div>
-            </div>
-            <div className="row gx-3 mb-3">
-                <div className="col-12">
+            <div className="center">
+                <div className="row">
                     <p><strong style={{ color: '#106544' }}>Success Stories:</strong></p>
                     <p>{PDFData.successStories}</p>
                 </div>
             </div>
-            <div className="row gx-3 mb-3">
-                <div className="col-12">
+            <div className="center">
+                <div className="row">
                     <p><strong style={{ color: '#106544' }}>Conclusions and Recommendations:</strong></p>
                     <p>{PDFData.conclusions}</p>
                 </div>
